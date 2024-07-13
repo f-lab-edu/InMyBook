e2ab94863d89fc2e70d9d8f9b9d46f152d0eb9f2
dani820 <danipark.st@gmail.com>
2024-07-14 00:47:54 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/inmybook/InmybookApplication.java
@@ -0,0 +1,13 @@
+package com.inmybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmybookApplication.class, args);
+	}
+
+}
