b48710799c65dd57366bad7fa843903f8c7b14f9
dani820 <danipark.st@gmail.com>
2024-08-03 19:00:20 +0900

    [#6] feat: 독서록 등록 기능 메서드 설명 주석 추가
    기능 설명, 파라미터, 리턴값 총 3가지 항목으로 작성

--- src/main/java/com/inmybook/application/port/in/RegisterPostUseCase.java
+++ src/main/java/com/inmybook/application/port/in/RegisterPostUseCase.java
@@ -1,5 +1,11 @@
 package com.inmybook.application.port.in;
 
 public interface RegisterPostUseCase {
+	/**
+	 * @NOTE 사용자가 작성한 독서록 게시글 정보를 등록한다.
+	 *
+	 * @param registerPostCommand
+	 * @return 독서록 상세 조회 API Path
+	 */
 	public String registerPost(RegisterPostCommand registerPostCommand);
 }
