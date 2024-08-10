0a808a1c1009bc1a5d1f1493a9b7e651454f5472
dani820 <danipark.st@gmail.com>
2024-08-10 19:58:07 +0900

    feat: 독서록 등록 기능 도메인 로직 변경에 따른 테스트 코드 수정
    썸네일 첨부한 경우와 첨부하지 않은 경우를 분리

--- src/test/java/com/inmybook/application/service/RegisterPostServiceTest.java
+++ src/test/java/com/inmybook/application/service/RegisterPostServiceTest.java
@@ -3,14 +3,16 @@ package com.inmybook.application.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.inmybook.adapter.in.web.dto.RegisterPostInputDto;
 import com.inmybook.application.port.in.RegisterPostCommand;
 import com.inmybook.application.port.out.RegisterPostPort;
 import com.inmybook.domain.post.Post;
@@ -29,57 +31,64 @@ class RegisterPostServiceTest {
 	PostFactory mockPostFactory;
 
 	@Test
-	@DisplayName("사용자는 독서록 게시글을 등록할 수 있다.")
-	void registerPostTest() {
+	@DisplayName("사용자는 독서록 게시글을 등록할 수 있다.(썸네일 존재하는 경우)")
+	void registerPostWithUploadImgTest() throws IOException {
 		// input : 책 정보, 유저 정보, 게시글 정보
 		// output : 상세 조회 url
-		
+
 		PostFactory postFactory = new PostFactory();
 
-		byte[] thumbnailData = new byte[0];
-		RegisterPostInputDto registerPostInputDto = new RegisterPostInputDto(
-			"9788966261208",
-			"HTTP 완벽 가이드",
-			"데이빗 골리 와 브라이언 토티",
-			"인사이트",
-			"HTTP 완벽 가이드 독서록",
-			"유익합니다.",
-			"2024-07-14",
-			"2024-07-21",
-			4.5,
-			0,
-			0,
-			"Y",
-			"dani820",
-			"원본파일명",
-			69835L,
-			"png",
-			thumbnailData
-		);
-
+		MultipartFile multipartFile = mock(MultipartFile.class);
 		RegisterPostCommand registerPostCommand = RegisterPostCommand.builder()
-			.isbnNo(registerPostInputDto.isbnNo())
-			.bookName(registerPostInputDto.bookName())
-			.author(registerPostInputDto.author())
-			.publisher(registerPostInputDto.publisher())
-			.title(registerPostInputDto.title())
-			.content(registerPostInputDto.content())
-			.fromDate(registerPostInputDto.fromDate())
-			.toDate(registerPostInputDto.toDate())
-			.rating(registerPostInputDto.rating())
-			.likeCount(registerPostInputDto.likeCount())
-			.bookmarkCount(registerPostInputDto.bookmarkCount())
-			.isPublic(registerPostInputDto.isPublic())
-			.memberId(registerPostInputDto.memberId())
-			.thumbnailName(registerPostInputDto.thumbnailName())
-			.thumbnailSize(registerPostInputDto.thumbnailSize())
-			.thumbnailExt(registerPostInputDto.thumbnailExt())
-			.thumbnailData(registerPostInputDto.thumbnailData())
+			.isbnNo("9788966261208")
+			.bookName("HTTP 완벽 가이드")
+			.author("데이빗 골리 와 브라이언 토티")
+			.publisher("인사이트")
+			.title("HTTP 완벽 가이드 독서록")
+			.content("유익합니다.")
+			.fromDate("2024-07-14")
+			.toDate("2024-07-21")
+			.rating(4.5)
+			.isPublic("Y")
+			.memberId("dani820")
+			.thumbnailImg(multipartFile)
 			.build();
 
 		Post post = postFactory.createPost(registerPostCommand);
-		String path = post.createPostPath(1);
 		when(mockPostFactory.createPost(registerPostCommand)).thenReturn(post);
+		String path = post.createPostPath(1);
+		when(registerPostPort.registerPost(post)).thenReturn(path);
+
+		assertThat(registerPostUseCase.registerPost(registerPostCommand)).isEqualTo(path);
+	}
+
+	@Test
+	@DisplayName("사용자는 독서록 게시글을 등록할 수 있다.(썸네일 존재하지 않는 경우)")
+	void registerPostWithoutUploadImgTest() throws IOException {
+		// input : 책 정보, 유저 정보, 게시글 정보
+		// output : 상세 조회 url
+
+		PostFactory postFactory = new PostFactory();
+
+		MultipartFile multipartFile = null;
+		RegisterPostCommand registerPostCommand = RegisterPostCommand.builder()
+			.isbnNo("9788966261208")
+			.bookName("HTTP 완벽 가이드")
+			.author("데이빗 골리 와 브라이언 토티")
+			.publisher("인사이트")
+			.title("HTTP 완벽 가이드 독서록")
+			.content("유익합니다.")
+			.fromDate("2024-07-14")
+			.toDate("2024-07-21")
+			.rating(4.5)
+			.isPublic("Y")
+			.memberId("dani820")
+			.thumbnailImg(multipartFile)
+			.build();
+
+		Post post = postFactory.createPost(registerPostCommand);
+		when(mockPostFactory.createPost(registerPostCommand)).thenReturn(post);
+		String path = post.createPostPath(1);
 		when(registerPostPort.registerPost(post)).thenReturn(path);
 
 		assertThat(registerPostUseCase.registerPost(registerPostCommand)).isEqualTo(path);
