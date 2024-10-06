2936963107553dd6380ec7add7da546971827235
dani820 <danipark.st@gmail.com>
2024-10-06 14:46:29 +0900

    feature: log4j2 적용 및 필터 구현

---
+++ src/main/java/com/inmybook/common/LogFilter.java
@@ -0,0 +1,38 @@
+package com.inmybook.common;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.slf4j.MDC;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogFilter extends OncePerRequestFilter {
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+
+		final UUID uuid = UUID.randomUUID();
+		MDC.put("requestId", uuid.toString());
+		MDC.put("userId", "dani820");
+
+		long startTime = System.currentTimeMillis();
+
+		log.info("{} start for request : {}", this.getClass().getSimpleName(), request.getRequestURI());
+
+		try {
+			filterChain.doFilter(request, response);
+		} finally {
+			long duration = System.currentTimeMillis() - startTime;
+			log.info("{} completed for request : {} - duration : {}ms", this.getClass().getSimpleName(),
+				request.getRequestURI(), duration);
+			MDC.clear();
+		}
+	}
+}
